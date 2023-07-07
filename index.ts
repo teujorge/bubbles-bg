6746922968fc1afef527285971b95482dfacc24e
Matheus Jorge <mrljorge@outlook.com>
2023-07-07 12:20:20 -0300

    new token

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import Bubbles from "./src/bubbles";
 
-// WORKFLOW_TOKEN : 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
+// WORKFLOW_TOKEN : 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
 
 export default Bubbles;
